dfde08fe5d0edc58b927e2ac9aedf357b4b30f3c
PTeresh <150271304+PTeresh@users.noreply.github.com>
2023-11-22 10:04:39 +0700

    main

---
+++ homework/TableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TablrViewControllerTwo.swift
+//  homework
+//
+//  Created by Павел Терешонок on 20.11.2023.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageAvatar: UIImageView!
+    @IBOutlet weak var nameOfUsers: UILabel!
+    @IBOutlet weak var lastMessage: UILabel!
+    @IBOutlet weak var timeOfLastMessage: UILabel!
+    
+    
+    
+    
+    
+    
+}
