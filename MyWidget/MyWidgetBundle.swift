63cc96ec731c77e6bf19707118a0f8e319d2506c
aleks100s <aleksandr100s@yandex.ru>
2023-12-02 19:28:26 +0700

    Add widget

---
+++ MyWidget/MyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MyWidgetBundle.swift
+//  MyWidget
+//
+//  Created by Alexander on 02.12.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyWidget()
+    }
+}
